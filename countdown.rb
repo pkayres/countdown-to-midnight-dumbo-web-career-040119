44d3a1d0cba864528a670025326dbf6b89848561
Pete A <peter.ayresmusic@gmail.com>
2019-03-06 00:10:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,10 +10,5 @@ return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(integer)
-  while integer > 0
-    puts 5.secs("#{integer} SECOND(S)!")
-    integer -= 1
-    break if integer == 0
-  end
-  return "HAPPY NEW YEAR!"
+  integer.secs
   end
